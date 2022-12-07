67e43fd413c58f5951cfd375ec40faf587fa47d7
henry nguyen <henry.nguyen@maptek.com.au>
2022-12-07 16:40:28 +1030

    Add files

--- 2022/main.py
+++ 2022/main.py
@@ -3,7 +3,13 @@ class Directory:
   def __init__(filename: str, parent_dir: Directory) -> None:
     self.filename = filename
     self.parent_dir = parent_dir
-    self.directories = []
-    self.files = []
+    self.directories = []   #Contains Directory objects only 
+    self.files = []   #Contains file sizes only 
 
 content = open("input").read().split('\n')
+content_id = 0
+
+root_dir = Directory("/", None)
+
+while(content_id < len(content)):
+  
