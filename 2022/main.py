9e78f283cf33f7cc868b8220bee7a567c5770c53
henry nguyen <henry.nguyen@maptek.com.au>
2022-12-07 15:47:51 +1030

    Working on day 7

--- 2022/main.py
+++ 2022/main.py
@@ -1,2 +1,9 @@
 # Template
+class Directory:
+  def __init__(filename: str, parent_dir: Directory) -> None:
+    self.filename = filename
+    self.parent_dir = parent_dir
+    self.directories = []
+    self.files = []
+
 content = open("input").read().split('\n')
