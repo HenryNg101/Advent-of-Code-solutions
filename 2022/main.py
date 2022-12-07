78586650f4324c3f7d2af4b4d8d6254f25662988
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2022-12-08 00:38:01 +1030

    Add day 7 solutions

--- 2022/main.py
+++ 2022/main.py
@@ -1,15 +1,2 @@
 # Template
-class Directory:
-  def __init__(filename: str, parent_dir: Directory) -> None:
-    self.filename = filename
-    self.parent_dir = parent_dir
-    self.directories = []   #Contains Directory objects only 
-    self.files = []   #Contains file sizes only 
-
 content = open("input").read().split('\n')
-content_id = 0
-
-root_dir = Directory("/", None)
-
-while(content_id < len(content)):
-  
