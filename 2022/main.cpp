1a3839ab2f6b52368691038dd9e1f439cfe06c27
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2022-12-09 01:01:35 +1030

    Add half of solutions for day 8

--- 2022/main.cpp
+++ 2022/main.cpp
@@ -4,21 +4,7 @@ using namespace std;
 //Template code
 int main()
 {
-    ifstream reader("input");
-    string s;
-    vector<vector<char>> st = {
-        {'Z','J','G'},
-        {'Q','L','R','P','W','F','V','C'},
-        {'F','P','M','C','L','G','R'},
-        {'L','F','B','W','P','H','M'},
-        {'G','C','F','S','V','Q'},
-        {'W','H','J','Z','M','Q','L','T'},
-        {'H','F','S','B','V'},
-        {'F','J','Z','S'},
-        {'M','C','D','P','F','H','B','T'}
-    };
-    while(getline(reader, s)){
-        
-    }
+    auto x = {1,2,3};
+    cout << typeof(x) << endl;
     return 0;
 }
