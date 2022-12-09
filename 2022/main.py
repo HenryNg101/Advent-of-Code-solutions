38a4a5d9e9072d9c3327283edc56a212a8188168
henry nguyen <henry.nguyen@maptek.com.au>
2022-12-09 17:01:17 +1030

    Add some files

--- 2022/main.py
+++ 2022/main.py
@@ -1,31 +1,33 @@
 # Template
 content = open("input").read().split('\n')
 content.pop()
-content = [[int(ch) for ch in line] for line in content]
 
-res = 0
+def check_position(row_H: int, col_H: int, row_T: int, col_T: int):
+    global visited
+    pass
 
-for r in range(len(content)):
-    for c in range(len(content[0])):
-        left = 0
-        for x in range(c-1, -1, -1):
-            left += 1
-            if content[r][x] >= content[r][c]: break
-        
-        right = 0
-        for x in range(c+1, len(content[0])):
-            right += 1
-            if content[r][x] >= content[r][c]: break
-        
-        up = 0
-        for x in range(r-1, -1, -1):
-            up += 1
-            if content[x][c] >= content[r][c]: break
+visited = [[False for _ in range(1000)] for _ in range(1000)]
+row_H = 500
+col_H = 500
+row_T = 500
+col_T = 500
 
-        down = 0
-        for x in range(r+1, len(content)):
-            down += 1
-            if content[x][c] >= content[r][c]: break
-        res = max(res, up * down * left * right)
+for line in content:
+    direction, moves = line.split(' ')
+    moves = int(moves)
 
-print(res)
+    if direction == 'U':
+        for _ in range(moves):
+            row_H -= 1
+
+    elif direction == 'D':
+        for _ in range(moves):
+            row_H += 1
+
+    elif direction == 'L':
+        for _ in range(moves):
+            col_H -= 1
+
+    elif direction == 'R':
+        for _ in range(moves):
+            col_H += 1
