0b49d464782efea52a85b5821692653392023d1b
henry nguyen <henry.nguyen@maptek.com.au>
2022-12-12 10:34:33 +1030

    Optimize day 9's solution

--- 2022/9.py
+++ 2022/9.py
@@ -1,12 +1,9 @@
-# Only applicable for this part, not for next part
-# Need to optimize later for next part
 content = open("input").read().split('\n')
 content.pop()
 
 def main(knots_count: int):
-    visited = [[False for _ in range(1000)] for _ in range(1000)]
-    knots = [[500, 500] for _ in range(knots_count)]
-    count = 0
+    visited = set()
+    knots = [[0, 0] for _ in range(knots_count)]
 
     for line in content:
         direction, moves = line.split(' ')
@@ -22,7 +19,7 @@ def main(knots_count: int):
             else:
                 knots[0][1] += 1
         
-            #Update new location of the tail (Will be extended for every positions)
+            #Update new location of the following knots, one by one
             for i in range(knots_count - 1):
                 cmp_r, cmp_c = knots[i][0] - knots[i+1][0], knots[i][1] - knots[i+1][1]
 
@@ -34,35 +31,27 @@ def main(knots_count: int):
                     knots[i+1][0] += cmp_r
                     knots[i+1][1] += cmp_c
 
-            #Check visited
-            if not visited[knots[-1][0]][knots[-1][1]]:
-                count += 1
-                visited[knots[-1][0]][knots[-1][1]] = True
+            visited.add(tuple(knots[-1]))
 
-    return count
+    return len(visited)
 
-print(f'Part a answer is: {main(2)}')
-print(f'Part b answer is: {main(10)}')
+print(f'Part A answer is: {main(2)}')
+print(f'Part B answer is: {main(10)}')
 
-""" . H H H .
-H . . . H
-H . T . H
-H . . . H
-. H H H .
+""" 
+a = rH - rT, b = cH - cT (H is head, T is tail)
 
-For diagonal moves, need to find out which quarter the new head is in
+All the cases when a knot need to move:
+    top left => -1, -2 or -2, -1 => rT + (-1), cT + (-1)
+    top right => -1, 2 or -2, 1 => rT + (-1), cT + 1 
+    bot left => 1, -2 or 2, -1 => rT + 1, cT + (-1)
+    bot right => 1, 2 or 2, 1 => rT + 1, cT + 1
+    left => 0, -2 => rT + 0, cT + (-1)
+    right => 0, 2 => rT + 0, cT + 1
+    top => -2, 0 => rT + (-1), cT + 0
+    bot => 2, 0 => rT + 1, cT + 0 
 
-a = rH - rT, b = cH - cT
-
-a /= abs(a)
-b /= abs(b)
-
-top left => -1, -2 or -2, -1 => rT + (-1), cT + (-1)
-top right => -1, 2 or -2, 1 => rT + (-1), cT + 1 
-bot left => 1, -2 or 2, -1 => rT + 1, cT + (-1)
-bot right => 1, 2 or 2, 1 => rT + 1, cT + 1
-
-left => 0, -2 => rT + 0, cT + (-1)
-right => 0, 2 => rT + 0, cT + 1
-top => -2, 0 => rT + (-1), cT + 0
-bot => 2, 0 => rT + 1, cT + 0 """
+From above pattern, we can see that, the formula to calculate where to move is:
+row = a // abs(a)
+col = b // abs(b)
+"""
