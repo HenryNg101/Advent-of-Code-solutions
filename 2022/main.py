0195dce830fa73daf3a2f81980856e368e44cda4
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2022-12-31 16:23:26 +1030

    Update README

--- 2022/main.py
+++ 2022/main.py
@@ -1,45 +1,9 @@
 import time
-import re
 
 start_time = time.process_time()
 content = open("input").read().split('\n')[:-1]     #Exclude the newline
 
-register = 0
-current_cycle = 0
-row, col = 0, 0
 
-crt = [['.'] * 40 for _ in range(6)]
-
-for line in content:
-    line = line.split()
-    if line[0] == 'noop':
-        #print(register)
-        if col in range(register, register + 3):
-            crt[row][col] = '#'
-        current_cycle += 1
-        col += 1
-        if col >= len(crt[0]):
-            row, col = row + 1, 0
-    else:
-        for _ in range(2):
-            #print(register)
-            if col in range(register, register + 3):
-                crt[row][col] = '#'
-            current_cycle += 1
-            col += 1
-            if col >= len(crt[0]):
-                row, col = row + 1, 0
-        register += int(line[1])
-
-for line in crt:
-    for char in line:
-        print(char, end='')
-    print()
 
 elapsed_time = (time.process_time() - start_time)
 print(f"Elapsed time: {elapsed_time * 1000} ms, {elapsed_time} seconds")
-
-'''
-X register controls horizontal pos of sprite
-The sprite is 3 pixels wide ??
-'''
--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 You can find each year solutions in the directory that has that year.
 
-Each day has 2 parts, so I separated them into part a and b. E.g: Day 4 has 2 files `4a.cpp` and `4b.cpp`
+Each day has 2 parts, so I separated them into part a and b, or merge them together (meaning that I have been able to to get solutions for both parts from one code file)
 
 How to run code for each challenge:
 
-- Download input file, save the file's name as `input` (no `.txt` extension) in same directory as the file you want to run.
+- Download input file, save the file's name as `input` (with out any extension) in same directory as the file you want to run.
 - Run command `g++ -o out <filename>.cpp; ./out` (for C++ files), `python3 <filename>.py` (for Python files)
+- Only files of days and parts I have done works, other just are drafts, pls don't run
+
+## AoC 2022 current stats
+
+![2022 stats](/2022/img/2022%20stats.png)
