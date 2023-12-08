6075b9ae8e759ca131e814bffb99ea14ea84275a
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2023-12-08 22:32:09 +1030

    Modify code for day 8 2023

--- 2023/8.py
+++ 2023/8.py
@@ -18,9 +18,9 @@ def required_steps(start_nodes: List[str], valid_end_nodes: Set[str]) -> List[in
 
     return required_steps_arr
 
-input = open("input").readlines()
+input = [line.strip() for line in open("input").readlines()]
 
-instructions = input[0][:-1]
+instructions = input[0]
 mp = dict()
 
 #Contains start and end nodes that satisfied requirements for part 2
@@ -29,9 +29,7 @@ start_nodes, valid_end_nodes = [], set()
 # Get start node, and left and right end nodes in each line
 for i in range(2, len(input)):
     start, dests = input[i].split(' = ')
-    left, right = dests.split()
-    left = left[1:-1]
-    right = right[:-1]
+    left, right = dests.strip('()').split(', ')
 
     mp[start] = (left, right)
     if start[-1] == 'A':
@@ -41,14 +39,6 @@ for i in range(2, len(input)):
     if right[-1] == 'Z':
         valid_end_nodes.add(right)
 
-# Part 1
-print(f"Part 1: {required_steps(['AAA'], set(['ZZZ']))[-1]}")
-
-# Part 2
-required_steps_arr = required_steps(start_nodes, valid_end_nodes)
-
-# Find LCM
-for i in range(1, len(required_steps_arr)):
-    required_steps_arr[i] = required_steps_arr[i] * required_steps_arr[i-1] // math.gcd(required_steps_arr[i], required_steps_arr[i-1])
-
-print(f"Part 2: {required_steps_arr[-1]}")
+# Print result. In part 2, calculate LCM for all numbers are calculated
+print(f"Part 1: {required_steps(['AAA'], {'ZZZ'})[0]}")
+print(f"Part 2: {math.lcm(*required_steps(start_nodes, valid_end_nodes))}")
