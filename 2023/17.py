7c167424d442ae2b9538b86ca417ed249b88fca6
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2023-12-18 08:57:24 +1030

    Optimize code for day 17 solution 2023

--- 2023/17.py
+++ 2023/17.py
@@ -4,94 +4,47 @@ import time
 # Start the timer
 start_time = time.time()
 
-class Node:
-    def __init__(self, r, c, cont_length, direction, cost: int):
-        self.r = r
-        self.c = c
-        self.cont_length = cont_length
-        self.direction = direction
-        self.cost = cost
-
-    def __lt__(self, other):
-        if self.cost == other.cost:
-            return self.cont_length < other.cont_length
-        return self.cost < other.cost
-
-input = [[int(val) for val in list(line.strip())] for line in open("input").readlines()]
-sz_r, sz_c = len(input), len(input[0])
-
 def dijkstra(min_blocks: int, max_blocks: int) -> int:
     shortest = [[1e9 for _ in range(sz_c)] for _ in range(sz_r)]
     visited = set()
 
-    pq = [Node(0, 0, 1, 'd', 0), Node(0, 0, 0, 'r', 0)]
+    # Each tuple's info follow this order: Cost, Continous length of the current direction, current row, current column, direction
+    # This helps the heap sort by cost, as the heap scans from left to right
+    pq = [(0, 0, 0, 0, 'd'), (0, 0, 0, 0, 'r')]
     heapify(pq)
-
     while pq:
-        node = heappop(pq)
-        r, c, cont_length, direction, cost = node.r, node.c, node.cont_length, node.direction, node.cost
+        cost, cont_length, r, c, direction = heappop(pq)
         shortest[r][c] = min(shortest[r][c], cost)
 
         if (r, c, cont_length, direction) in visited:
             continue
         visited.add((r, c, cont_length, direction))
 
-        if direction == 'd':
-            # Go straight
-            if r + 1 < sz_r and cont_length < max_blocks:
-                heappush(pq, Node(r+1, c, cont_length+1, direction, cost + input[r+1][c]))
+        # For the case of keep the same direction
+        move_r, move_c = next_moves[direction]
+        next_r, next_c = r + move_r, c + move_c
 
-            if cont_length >= min_blocks:
-                # Turn left
-                if c < sz_c -1:
-                    heappush(pq, Node(r, c+1, 1, 'r', cost + input[r][c+1]))
+        if 0 <= next_r < sz_r and 0 <= next_c < sz_c and cont_length < max_blocks:
+            heappush(pq, (cost + input[next_r][next_c], cont_length + 1, next_r, next_c, direction))
+        
+        # For the case of left and right turn from original direction
+        for turn in future_turns[direction]:
+            move_r, move_c = next_moves[turn]
+            next_r, next_c = r + move_r, c + move_c
 
-                # Turn right
-                if c > 0:
-                    heappush(pq, Node(r, c-1, 1, 'l', cost + input[r][c-1]))
+            if 0 <= next_r < sz_r and 0 <= next_c < sz_c and cont_length >= min_blocks:
+                heappush(pq, (cost + input[next_r][next_c], 1, next_r, next_c, turn))
 
-        elif direction == 'u':
-            # Go straight
-            if r > 0 and cont_length < max_blocks:
-                heappush(pq, Node(r-1, c, cont_length+1, direction, cost + input[r-1][c]))
-
-            if cont_length >= min_blocks:
-                # Turn left
-                if c > 0:
-                    heappush(pq, Node(r, c-1, 1, 'l', cost + input[r][c-1]))
-
-                # Turn right
-                if c < sz_c -1:
-                    heappush(pq, Node(r, c+1, 1, 'r', cost + input[r][c+1]))
-
-        elif direction == 'r':
-            # Go straight
-            if c + 1 < sz_c and cont_length < max_blocks:
-                heappush(pq, Node(r, c+1, cont_length+1, direction, cost + input[r][c+1]))
-
-            if cont_length >= min_blocks:
-                # Turn left
-                if r > 0:
-                    heappush(pq, Node(r-1, c, 1, 'u', cost + input[r-1][c]))
-
-                # Turn right
-                if r < sz_r - 1:
-                    heappush(pq, Node(r+1, c, 1, 'd', cost + input[r+1][c]))
-
-        elif direction == 'l':
-            # Go straight
-            if c > 0 and cont_length < max_blocks:
-                heappush(pq, Node(r, c-1, cont_length+1, direction, cost + input[r][c-1]))
-
-            if cont_length >= min_blocks:
-                # Turn right
-                if r > 0:
-                    heappush(pq, Node(r-1, c, 1, 'u', cost + input[r-1][c]))
-
-                # Turn left
-                if r < sz_r - 1:
-                    heappush(pq, Node(r+1, c, 1, 'd', cost + input[r+1][c]))
     return shortest[-1][-1]
+
+input = [[int(val) for val in list(line.strip())] for line in open("input").readlines()]
+sz_r, sz_c = len(input), len(input[0])
+
+# Directions that are considered left and right, from the current direction
+future_turns = {'d': 'rl', 'u':'lr', 'l':'ud', 'r': 'du'}
+
+# Next location, based on direction
+next_moves = {'d': (1, 0), 'u': (-1, 0), 'l': (0, -1), 'r': (0, 1)}
 
 print(f'Part 1: {dijkstra(0, 3)}')
 print(f'Part 2: {dijkstra(4, 10)}')
