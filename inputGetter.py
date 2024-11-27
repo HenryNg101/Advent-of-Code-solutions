23c9eb9b182ad89242253f2909c26dbd3663bb01
Henry Nguyen <hungnguyenquoc8a2@gmail.com>
2024-11-27 17:41:26 +1030

    Add solution files for day 1-15 (except day 11) of year 2017, and update cookie for data retrieval

--- inputGetter.py
+++ inputGetter.py
@@ -2,7 +2,10 @@ import requests
 
 #Change the value of the "session" key to your own key
 COOKIES = {
-    'session': '53616c7465645f5f67e747e4ec15f285204a54730944591d46e3567ae7d7fe60bcd4f2a155884944f904f56e184bfed319e1b109b2f5f896876591e936b9c62d'
+    '_ga': 'GA1.2.1887550309.1732287497',
+    '_gid': 'GA1.2.756549688.1732455219',
+    '_ga_MHSNPJKWC7': 'GS1.2.1732458150.3.1.1732459421.0.0.0',
+    'session': '53616c7465645f5fb82426dd9637e3176d39889b657ad53cc5f10b09e403ac8c85ae8c986570dbd0d6517b21592d984245bc3fe0436b9f6165b776589558d8af'
 }
 
 def get_input(year: str, day: str):
