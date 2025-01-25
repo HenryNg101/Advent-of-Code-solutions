76be7ede7fc4aff609ab3af335ad1a7644e2ba9e
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2025-01-25 18:42:04 +0700

    Optimize Rust code for day 15 year 2025

--- rust/src/bin/2015-13.rs
+++ rust/src/bin/2015-13.rs
@@ -1,40 +1,17 @@
 use std::{cmp::max, collections::{HashMap, HashSet}, fs, i32};
-
-fn permutations<'a>(
-    id: usize, peoples: &mut Vec<&'a str>, 
-    result: &mut Vec<Vec<&'a str>>) {
-    
-    if id >= peoples.len() {
-        result.push(peoples.to_vec());
-        return;
-    }
-    for i in id..peoples.len() {
-        peoples.swap(i, id);
-        permutations(id+1, peoples, result);
-        peoples.swap(i, id);
-    }
-}
+use itertools::Itertools;
 
 fn optimal_hapiness(peoples: &HashSet<&str>, mp: &HashMap<(&str, &str), i32>) -> i32 {
     let mut max_dis = i32::MIN;
-    let mut result : Vec<Vec<&str>> = Vec::new();
-    permutations(0, &mut Vec::from_iter(peoples.clone()), &mut result);
-
-    for perm in result {
+    for perm in peoples.iter().permutations(peoples.len()) {
         let mut total_dis = 0;
-        for (i, &elem) in perm.iter().enumerate() {
-            let j = if i > 0 {
-                i - 1
-            } else {
-                perm.len() - 1
-            };
-            let (start, stop) = (perm[j], elem);
+        for i in 0..perm.len() {
+            let (start, stop) = (perm[i], perm[(i+1) % perm.len()]);
             total_dis += mp.get(&(start, stop)).unwrap() + mp.get(&(stop, start)).unwrap();
         }
         max_dis = max(max_dis, total_dis);
     }
-
-    return max_dis;
+    max_dis
 }
 
 fn main() {
@@ -46,12 +23,10 @@ fn main() {
 
     for line in content.lines() {
         let line: Vec<&str> = line.split_whitespace().collect();
-        let (start, stop, cost) = (
-            line[0], *line.last().unwrap(), line[3]);
-        let stop = &stop[..stop.len()-1];
-        let mut cost = cost.parse::<i32>().unwrap();
+        let (start, stop) = (line[0], line.last().unwrap().trim_end_matches('.'));
+        let mut cost = line[3].parse::<i32>().unwrap();
         if line[2] == "lose" {
-            cost = 0 - cost;
+            cost = -cost;
         }
 
         peoples.insert(start);
@@ -61,10 +36,10 @@ fn main() {
     println!("Part A: {}", optimal_hapiness(&peoples, &mp));
 
     // Part B
-    for &people in &peoples {
-        mp.insert((people, "you"), 0);
-        mp.insert(("you", people), 0);
-    }
+    peoples.iter().for_each(|&person| {
+        mp.insert((person, "you"), 0);
+        mp.insert(("you", person), 0);
+    });
     peoples.insert("you");
     println!("Part B: {}", optimal_hapiness(&peoples, &mp));
 }
